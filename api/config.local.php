a08a0e7faf0ac04928c948d7e31579e0103f69b0
Dziadosz Wiktor <57013875+DziadoszWiktor@users.noreply.github.com>
2025-11-16 22:15:59 +0100

    Update config.local.php

--- api/config.local.php
+++ api/config.local.php
@@ -3,11 +3,11 @@
 // LOKALNA konfiguracja – tego pliku NIE wrzucamy do gita (dodany do .gitignore)
 
 return [
-    'db_host'   => 'fdb1034.awardspace.net',
+    'db_host'   => '',
     'db_port'   => 3306,
-    'db_name'   => '4696539_wddb',
-    'db_user'   => '4696539_wddb',
-    'db_pass'   => 'eEv#}7q;48.rYl(p',
+    'db_name'   => '',
+    'db_user'   => '',
+    'db_pass'   => '',
 
     // opcjonalnie: w dev = true, na prod można dać false
     'display_errors' => true,
